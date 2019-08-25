837b44a1143fc1bac9d067bec451df1bf85710e3
eboy02wangmx <zzm798681538@126.com>
2019-08-25 12:51:39 +0900

    zhang

--- myapp-backend/src/main/java/jp/co/myapp/api/app/controller/LoginController.java
+++ myapp-backend/src/main/java/jp/co/myapp/api/app/controller/LoginController.java
@@ -1,9 +1,14 @@
 package jp.co.myapp.api.app.controller;
 
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -59,5 +64,47 @@ public class LoginController extends AbstractApiController {
 		return configBean;
 	}
 
+	/**
+     * ファイルアップロード処理
+     *
+     * @param request
+     *            HttpServletRequest
+     * @param response
+     *            HttpServletResponse
+     * @throws Exception
+     */
+    @RequestMapping(value = "/api/imageUpload", method = { RequestMethod.POST })
+    @ResponseBody
+    public void fileupload(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        logger.info("NHA_O_0011" + " 処理開始");
 
+        Part part = request.getPart("file");
+        String fileName = part.getSubmittedFileName();
+
+        LoginController lc = new LoginController();
+        String path = lc.getClass().getResource("/").getPath();
+        path = path.substring(1, path.indexOf("classes")) + "uploads";
+
+        Path outputDir = Paths.get(path, request.getSession().getId());
+        if (!Files.exists(outputDir)) {
+            Files.createDirectory(outputDir);
+        }
+
+        // ファイルが重複している場合409を返す
+        if (Files.exists(outputDir.resolve(fileName))) {
+            response.sendError(HttpServletResponse.SC_CONFLICT, "ファイル名が重複しています");
+            return;
+        }
+
+        try (InputStream is = part.getInputStream()) {
+            Files.copy(is, outputDir.resolve(fileName));
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+
+        response.setCharacterEncoding("utf-8");
+        response.setContentType("application/json");
+
+        logger.info("NHA_O_0011" + " POST処理完了");
+    }
 }
