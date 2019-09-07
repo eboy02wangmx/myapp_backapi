fc72e8fd7e6c01da2d33b488b717e8b60da7e659
eboy02wangmx <lsy1285362801@gmail.com>
2019-09-07 13:13:08 +0900

    書式対応

--- myapp-backend/src/main/java/jp/co/myapp/api/app/controller/AlbumController.java
+++ myapp-backend/src/main/java/jp/co/myapp/api/app/controller/AlbumController.java
@@ -7,15 +7,17 @@ import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import jp.co.myapp.api.app.request.UserRequest;
+import jp.co.myapp.api.app.request.AlbumRequest;
 import jp.co.myapp.api.domain.model.AlbumBean;
 import jp.co.myapp.api.domain.service.album.AlbumService;
 
+@Controller
 public class AlbumController {
 	@Inject
 	AlbumService albumService;
@@ -23,8 +25,10 @@ public class AlbumController {
 	@RequestMapping(value = "/api/album", method = { RequestMethod.POST })
 	@ResponseBody
 	public List<AlbumBean> getAlbumInfo(HttpServletRequest request, HttpServletResponse response, Locale locale,
-			@RequestBody UserRequest params) {
-		String user = request.getParameter("userid");
-		return albumService.getAlbum(user);
+			@RequestBody AlbumRequest params) {
+		String userid = params.getUserid();
+		List<AlbumBean> albums = albumService.getAlbum(userid);
+		return albums;
+
 	}
 }
--- myapp-backend/src/main/java/jp/co/myapp/api/app/controller/AlbumCreateController.java
+++ myapp-backend/src/main/java/jp/co/myapp/api/app/controller/AlbumCreateController.java
@@ -1,21 +1,30 @@
 package jp.co.myapp.api.app.controller;
 
+import java.util.Locale;
+
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import jp.co.myapp.api.domain.service.albumcreate.AlbumCreateService;
+import jp.co.myapp.api.app.request.AlbumCreateRequest;
+import jp.co.myapp.api.domain.service.album.AlbumCreateService;
 
+@Controller
 public class AlbumCreateController {
 	@Inject
 	AlbumCreateService albumCreateService;
 
-	@RequestMapping(value = "/api/albumCreate", method = { RequestMethod.GET })
-	public void albumCreate(HttpServletRequest request, HttpServletResponse response) {
-		String user1 = request.getParameter("user");
-		albumCreateService.create(user1);
+	@RequestMapping(value = "/api/albumCreaet", method = { RequestMethod.POST })
+	@ResponseBody
+	public void getAlbumInfo(HttpServletRequest request, HttpServletResponse response, Locale locale,
+			@RequestBody AlbumCreateRequest params) {
+		albumCreateService.albumCreate(params);
+
 	}
 }
